e7a10628a86006927b04c883df86afab237fd680
ManaGh <managhabraei2022@gmail.com>
2022-12-23 15:38:56 +0000

    7

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 let Client = 4517;
 let DevFirstName = "Mana";
-let devFamilyName = "Ghabraei"
-let degree = "Master"
+let devFamilyName = "Ghabraei";
+let degree = "Master";
 let myAge = 27;
 let Goal = "Be a professional";
 let InMinutes = 54384;
 let isOnline = true;
-let firstNumber = 25
-let secNumber = 26784
-let remainder = 26784 % 25
+let firstNumber = 25;
+let secNumber = 26784;
+let remainder = 26784 % 25;
+let number = 12
+let str = 12
+let number2 = 14
+console.log(number == number2);
+console.log(number == str);
 console.log(remainder);
-console.log("About creator" +
-    DevFirstName + " " + devFamilyName + "\n" + "She has:" +
-    degree);
+console.log(
+    "About creator" +
+    DevFirstName +
+    " " +
+    devFamilyName +
+    "\n" +
+    "She has:" +
+    degree +
+    " " +
+    ' in " physical-chemistry"'
+);
 console.log(typeof isOnline);
 console.log(Goal);
 console.log(Client);
@@ -20,4 +33,4 @@ console.log(Client);
 console.log(InMinutes);
 
 console.log(typeof myAge);
-console.log((firstNumber) + (secNumber));
+console.log(firstNumber + secNumber);
