bac9f998a441524717cc244baab12a4d5c5f65af
ManaGh <managhabraei2022@gmail.com>
2022-12-23 13:59:00 +0000

    7

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 let Client = 4517;
-let Developer = "Mana";
+let DevFirstName = "Mana";
+let devFamilyName = "Ghabraei"
+let degree = "Master"
 let myAge = 27;
 let Goal = "Be a professional";
 let InMinutes = 54384;
 let isOnline = true;
 let firstNumber = 25
 let secNumber = 26784
-
+let remainder = 26784 % 25
+console.log(remainder);
+console.log("About creator" +
+    DevFirstName + " " + devFamilyName + "\n" + "She has:" +
+    degree);
 console.log(typeof isOnline);
 console.log(Goal);
 console.log(Client);
