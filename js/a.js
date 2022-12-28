09fd6cd5898b32ebb5d4e20748ec5f4decdf5c0a
ManaGh <managhabraei2022@gmail.com>
2022-12-28 12:35:14 +0000

    11

--- js/a.js
+++ js/a.js
@@ -1,6 +1,6 @@
 function solution(D, S) {
     let arr = ["one", "two", "three", "four", "five"];
-    if (D >= 1 && D <= 5 && arr.index(S) !== -1) {
-        returnD * (arr.index(S) + 1);
+    if (D >= 1 && D <= 5 && arr.indexOf(S) !== -1) {
+        returnD * (arr.indexOf(S) + 1);
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,43 +1,83 @@
-let Client = 4517;
-let DevFirstName = "Mana";
-let devFamilyName = "Ghabraei";
-let degree = "Master";
-let myAge = 27;
-let Goal = "Be a professional";
-let InMinutes = 54384;
-let isOnline = true;
-let firstNumber = 25;
-let secNumber = 26784;
-let remainder = 26784 % 25;
-let number = 12
-let str = "12"
-let number2 = 14
-console.log(number == number2);
-console.log(number == str);
-console.log(number === str);
-console.log(remainder);
-console.log(
-    "About creator" +
-    DevFirstName +
-    " " +
-    devFamilyName +
-    "\n" +
-    "She has:" +
-    degree +
-    " " +
-    ' in " physical-chemistry"'
-);
-console.log(typeof isOnline);
-console.log(Goal);
-console.log(Client);
+/*important *** I used  "Live server" for see the result when I used alert/ *
+// let Client = 4517;
+// let DevFirstName = "Mana";
+// let devFamilyName = "Ghabraei";
+// let degree = "Master";
+// let myAge = 27;
+// let Goal = "Be a professional";
+// let InMinutes = 54384;
+// let isOnline = true;
+// let firstNumber = 25;
+// let secNumber = 26784;
+// let remainder = 26784 % 25;
+// let number = 12
+// let str = "12"
+// let number2 = 14
+// console.log(number == number2);
+// console.log(number == str);
+// console.log(number === str);
+// console.log(remainder);
+// console.log(
+//     "About creator" +
+//     DevFirstName +
+//     " " +
+//     devFamilyName +
+//     "\n" +
+//     "She has:" +
+//     degree +
+//     " " +
+//     ' in " physical-chemistry"'
+// );
+// console.log(typeof isOnline);
+// console.log(Goal);
+// console.log(Client);
 
-console.log(InMinutes);
+// console.log(InMinutes);
 
-console.log(typeof myAge);
-console.log(firstNumber + secNumber);
+// console.log(typeof myAge);
+// console.log(firstNumber + secNumber);
 
-let students = ["Mana", "Saghar", "Leila", "Bita"];
+// let students = ["Mana", "Saghar", "Leila", "Bita"];
 
-console.log(students);
-console.log(students.length);
-console.log(students[2]);
+// console.log(students);
+// console.log(students.length);
+// console.log(students[2]);
+
+// let num1 = 30;
+// let num2 = 6;
+// let num3 = 5;
+// if (num2 > num3) {
+//     alert("yes");
+// } 
+
+//if x was "true" do & if it was not true do else
+
+// let num1 = 30;
+// let num2 = 6;
+// let num3 = 5;
+// if (num2 < num3) {
+//     alert("Yes");
+
+// } else {
+//     alert("No");
+// }
+
+/*syntax if:
+if(){
+    
+}else{
+
+}*/
+
+
+let num1 = 12
+let num2 = 4
+let num3 = 3
+if (num2 * num3 === 10) {
+    alert("multiply of them is equal 10");
+
+} else if (num2 * num3 === 11) {
+    alert("multiply of them is equal 11");
+} else if (num2 * num3 === 12) {
+    alert("multiply of them is equal 12");
+}
